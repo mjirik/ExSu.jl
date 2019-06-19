746395c919d5684b6ed18090271e9f36636c3d82
Miroslav Jirik <miroslav.jirik@gmail.com>
2019-06-19 12:12:36 +0200

    new package added

---
+++ test/experiment_support_test.jl
@@ -0,0 +1,17 @@
+using Revise
+using Test
+using Logging
+using ExSu
+# using LarSurf
+# Logging.configure(level==Logging.Debug)
+
+
+# include("../src/LarSurf.jl")
+# include("../src/block.jl")
+
+@testset "to_csv" begin
+    ExSu.add_to_csv((:jedna=>[1], :dva=>[2]), "test1.csv")
+    @test isfile("test1.csv")
+    ExSu.add_to_csv((:jedna=>1, :dva=>2), "test2.csv")
+    @test isfile("test2.csv")
+end
